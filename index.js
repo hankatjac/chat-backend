ccd19a55028c5c29817a139b9918a62f23ca4345
Hank <hangryan@gmail.com>
2023-03-26 02:14:19 -0400

    1

--- index.js
+++ index.js
@@ -4,57 +4,54 @@ const app = express();
 
 const httpServer = require("http").createServer(app);
 
-
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Exel-Tech chat application." });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 50000;
 
-httpServer.listen(PORT || 5000, () => console.log(`Server is running on port ${PORT}.`));
+httpServer.listen(PORT || 50000, () =>
+  console.log(`Server is running on port ${PORT}.`)
+);
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "https://hankatjac.github.io",
-    methods: ["GET", "POST"]
-  }
+    origin: ["https://www.exel-tech.com", "http://localhost:8081"],
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', socket => {
-  const id = socket.handshake.query.id
-  socket.join(id)
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
 
-  if (id == 'Admin') {
-    socket.broadcast.emit('admin-connected')
-  }
-  else {
-    if (socket.adapter.rooms.has('Admin')) {
-      io.to(id).emit('admin-connected')
+  if (id == "Admin") {
+    socket.broadcast.emit("admin-connected");
+  } else {
+    if (socket.adapter.rooms.has("Admin")) {
+      io.to(id).emit("admin-connected");
+    } else {
+      io.to(id).emit("admin-disconnected");
     }
-    else {
-      io.to(id).emit('admin-disconnected')
-    }
-    socket.broadcast.to('Admin').emit('user-connected', id)
+    socket.broadcast.to("Admin").emit("user-connected", id);
   }
 
+  socket.on("send-message", ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit("receive-message", {
+        recipients: newRecipients,
+        sender: id,
+        text,
+      });
+    });
+  });
 
-  socket.on('send-message', ({ recipients, text }) => {
-    recipients.forEach(recipient => {
-      const newRecipients = recipients.filter(r => r !== recipient)
-      newRecipients.push(id)
-      socket.broadcast.to(recipient).emit('receive-message', {
-        recipients: newRecipients, sender: id, text
-      })
-    })
-  })
-
-  socket.on('disconnect', () => {
-
-    if (id == 'Admin') {
-      socket.broadcast.emit('admin-disconnected')
+  socket.on("disconnect", () => {
+    if (id == "Admin") {
+      socket.broadcast.emit("admin-disconnected");
     }
-    socket.broadcast.to('Admin').emit('user-disconnected', id)
-  })
-
-})
-
+    socket.broadcast.to("Admin").emit("user-disconnected", id);
+  });
+});
